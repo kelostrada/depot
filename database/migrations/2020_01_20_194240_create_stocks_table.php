6cefd03a1a9def743d0699bbc47344e62f8f2e37
kelostrada <bartek@gamecodehq.com>
2020-01-20 20:49:19 +0100

    add stocks

---
+++ database/migrations/2020_01_20_194240_create_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->decimal('price_buy', 8, 2);
+            $table->decimal('price_sell', 8, 2);
+            $table->string('invoice');
+            $table->date('buy_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
