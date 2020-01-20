bf13e90e243ea650cc6b8e39561b146e961d1b0e
kelostrada <bartek@gamecodehq.com>
2020-01-20 20:22:50 +0100

    Add api token and React component reading auth api calls

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+class Example extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {user: {}};
+    }
 
-                        <div className="card-body">I'm an example component!</div>
+    componentDidMount() {
+        const token = $('meta[name=token]')[0].content;
+
+        axios.get('/api/user', {
+            headers: {'Authorization': `Bearer ${token}`}
+        }).then(response => {
+            const user = response.data;
+            this.setState({ user });
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+
+                            <div className="card-body">{ this.state.user.id } { this.state.user.name }</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Example;
