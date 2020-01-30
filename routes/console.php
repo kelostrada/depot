0fd1f0e9a6bd9b9319b3e0b16845ea0fd6099f08
kelostrada <bartek@gamecodehq.com>
2020-01-30 13:07:00 +0100

    Fetch rates from NBP

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use GuzzleHttp\Client;
+use Carbon\Carbon;
+use \App\Models\Rate;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,37 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+function fetchRates($date)
+{
+    $client = new Client();
+    $res = $client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/a/' . $date, [
+        'headers' => [
+            'Accept' => 'application/json'
+        ],
+        'http_errors' => false
+    ]);
+
+    if ($res->getStatusCode() < 300) {
+        $body = $res->getBody();
+        $response = json_decode($body);
+        foreach ($response[0]->rates as $fetchedRate) {
+            $rate = new Rate();
+            $rate->currency = $fetchedRate->code;
+            $rate->value = $fetchedRate->mid;
+            $rate->date = $date;
+            $rate->save();
+        }
+    }
+}
+
+Artisan::command('fetch_rates {date}', function($date) {
+    $this->comment('Fetching rates...');
+
+    $date = new Carbon($date);
+    while(Carbon::now()->diffInDays($date) > 0) {
+        $this->comment("Fetching " . $date->toDateString());
+        fetchRates($date->toDateString());
+        $date->addDay();
+    }
+})->describe('Rates fetch from NBP');
